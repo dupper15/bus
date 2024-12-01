8fe81c1df743d4e01397955fc1f77a8c51260b83
Nhật Nguyễn Lê Tuấn <22521019@ms.uit.edu.vn>
2024-12-01 19:44:46 +0700

    Change title

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,14 +3,14 @@ import logo from "../../assets/Screenshot 2024-11-24 190925.png";
 
 const Footer = () => {
   return (
-    <div className='h-10 w-screen p-2 flex justify-between items-center bg-[#4CAF50]'>
-      <div className='flex gap-4 text-white text-lg font-normal ml-8'>
+    <div className="h-10 w-screen p-2 flex justify-between items-center bg-[#4CAF50]">
+      <div className="flex gap-4 text-white text-lg font-normal ml-8">
         <div>Cam Dam Dam</div>
         <div>Legal</div>
         <div>Privacy</div>
       </div>
-      <div className='flex gap-4 text-white text-xl font-normal'>
-        <img src={logo} className='h-8 mr-8' />
+      <div className="flex gap-4 text-white text-xl font-normal">
+        <img src={logo} className="h-8 mr-8" />
       </div>
     </div>
   );
