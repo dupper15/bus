41c0d672f4e739490791de620f9346f54859ca10
dupper15 <nguyenchinghia15@gmail.com>
2024-12-29 20:18:00 +0700

    feat check in

--- backend/controllers/ScheduleController.js
+++ backend/controllers/ScheduleController.js
@@ -1,104 +1,138 @@
-const ScheduleService = require('../services/ScheduleService')
-require("../services/OpinionService");
-const createSchedule =  async (req, res) => {
-    try {
-        const data = req.body;
-        const response = await ScheduleService.createSchedule(data)
-        return res.status(201).json(response)
-    } catch (e) {
-        console.error(e)
-        return res.status(500).json({
-            status: 'ERROR',
-            message: 'An error occurred while creating the schedule.',
-            error: e
-        })
+const ScheduleService = require("../services/ScheduleService");
+const createSchedule = async (req, res) => {
+  try {
+    const data = req.body;
+    const response = await ScheduleService.createSchedule(data);
+    return res.status(201).json(response);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({
+      status: "ERROR",
+      message: "An error occurred while creating the schedule.",
+      error: e,
+    });
+  }
+};
+const getDetailSchedule = async (req, res) => {
+  try {
+    const ScheduleId = req.params.id;
+    if (!ScheduleId) {
+      return res.status(400).json({
+        status: "ERROR",
+        message: "Schedule ID is required.",
+      });
     }
-}
+    const response = await ScheduleService.getDetailSchedule(ScheduleId);
+    return res.status(200).json(response);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({
+      status: "ERROR",
+      message: "An error occurred while retrieving the schedule details.",
+      error: e,
+    });
+  }
+};
 
-const getDetailSchedule =  async (req, res) => {
-    try {
-        const ScheduleId = req.params.id
-        if (!ScheduleId){
-            return res.status(400).json({
-                status: 'ERROR',
-                message: 'Schedule ID is required.'
-            })
-        }
-        const response = await ScheduleService.getDetailSchedule(ScheduleId)
-        return res.status(200).json(response)
-    } catch (e) {
-        console.error(e)
-        return res.status(500).json({
-            status: 'ERROR',
-            message: 'An error occurred while retrieving the schedule details.',
-            error: e
-        })
+const getEmployeeTask = async (req, res) => {
+  try {
+    const employeeId = req.params.id;
+    if (!employeeId) {
+      return res.status(400).json({
+        status: "ERROR",
+        message: "employeeId is required.",
+      });
     }
-}
+    const response = await ScheduleService.getEmployeeTask(employeeId);
+    return res.status(200).json(response);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({
+      status: "ERROR",
+      message: "An error occurred while retrieving the employeeId",
+      error: e,
+    });
+  }
+};
 
 const updateSchedule = async (req, res) => {
-    try {
-        const data = req.body
-        const response = await ScheduleService.updateSchedule(data)
-        return res.status(200).json(response)
-    } catch (e) {
-        console.error(e)
-        return res.status(500).json({
-            status: 'ERROR',
-            message: 'An error occurred while updating the schedule.'
-        })
-    }
-}
+  try {
+    const data = req.body;
+    const response = await ScheduleService.updateSchedule(data);
+    return res.status(200).json(response);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({
+      status: "ERROR",
+      message: "An error occurred while updating the schedule.",
+    });
+  }
+};
 
-const getAllSchedule =  async (req, res) => {
-    try {
-        const response = await ScheduleService.getAllSchedule()
-        return res.status(200).json(response)
-    } catch (e) {
-        console.error(e)
-        return res.status(500).json({
-            status: 'ERROR',
-            message: 'An error occurred while retrieving the schedules.',
-            error: e
-        })
-    }
-}
+const getAllSchedule = async (req, res) => {
+  try {
+    const response = await ScheduleService.getAllSchedule();
+    return res.status(200).json(response);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({
+      status: "ERROR",
+      message: "An error occurred while retrieving the schedules.",
+      error: e,
+    });
+  }
+};
 
+const getAllAdd = async (req, res) => {
+  try {
+    const response = await ScheduleService.getAllAdd();
+    return res.status(200).json(response);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({
+      status: "ERROR",
+      message: "An error occurred while retrieving the schedules.",
+      error: e,
+    });
+  }
+};
 
-const getAllAdd =  async (req, res) => {
-    try {
-        const response = await ScheduleService.getAllAdd()
-        return res.status(200).json(response)
-    } catch (e) {
-        console.error(e)
-        return res.status(500).json({
-            status: 'ERROR',
-            message: 'An error occurred while retrieving the schedules.',
-            error: e
-        })
-    }
-}
-
-const deleteSchedule =  async (req, res) => {
-    try {
-        const ScheduleId = req.params.id
-        const response = await ScheduleService.deleteSchedule(ScheduleId)
-        return res.status(200).json(response)
-    } catch (e) {
-        console.error(e)
-        return res.status(500).json({
-            status: 'ERROR',
-            message: 'An error occurred while deleting the schedule.',
-            error: e
-        })
-    }
-}
-
+const deleteSchedule = async (req, res) => {
+  try {
+    const ScheduleId = req.params.id;
+    const response = await ScheduleService.deleteSchedule(ScheduleId);
+    return res.status(200).json(response);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({
+      status: "ERROR",
+      message: "An error occurred while deleting the schedule.",
+      error: e,
+    });
+  }
+};
+const employeeCheckIn = async (req, res) => {
+  try {
+    const data = req.body;
+    console.log(data, "here");
+    const response = await ScheduleService.employeeCheckIn(data);
+    return res.status(200).json(response);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({
+      status: "ERROR",
+      message: "An error occurred while checking in.",
+      error: e,
+    });
+  }
+};
 module.exports = {
-    createSchedule,
-    getDetailSchedule,
-    updateSchedule,
-    getAllSchedule,
-    deleteSchedule,
-    getAllAdd
-}
+  createSchedule,
+  getDetailSchedule,
+  updateSchedule,
+  getAllSchedule,
+  deleteSchedule,
+  getEmployeeTask,
+  employeeCheckIn,
+  getAllAdd,
+};
--- frontend/src/pages/Employee/TaskPage/TaskPage.jsx
+++ frontend/src/pages/Employee/TaskPage/TaskPage.jsx
@@ -1,5 +1,5 @@
 import Search from "@/components/ui/search";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Table,
   TableBody,
@@ -19,44 +19,76 @@ import {
   PaginationPrevious,
 } from "@/components/ui/pagination";
 import CheckInForm from "@/components/SmallForm/CheckInForm";
-const items = [
-  {
-    id: "L001",
-    name: "01",
-    start_place: "võ văn ngân",
-    end_place: "nguyễn thị minh khai",
-    time: "1 tiếng 15p",
-  },
-  {
-    id: "L002",
-    name: "02",
-    start_place: "Tân Sơn Nhất",
-    end_place: "Bến Thành",
-    time: "45 phút",
-  },
-  {
-    id: "L003",
-    name: "03",
-    start_place: "Bến Thành",
-    end_place: "Chợ Lớn",
-    time: "1 tiếng",
-  },
-  {
-    id: "L004",
-    name: "04",
-    start_place: "Chợ Lớn",
-    end_place: "Gò Vấp",
-    time: "1 tiếng 20 phút",
-  },
-  {
-    id: "L005",
-    name: "05",
-    start_place: "Bình Thạnh",
-    end_place: "Quận 7",
-    time: "50 phút",
-  },
-];
+import { useMutation } from "@tanstack/react-query";
+import * as ScheduleService from "@/services/scheduleService";
+import { useSelector } from "react-redux";
+import { set } from "date-fns";
+
 const TaskPage = () => {
+  const [taskItems, setTaskItems] = useState([]);
+  const items = [
+    {
+      id: "L001",
+      name: "01",
+      start_place: "võ văn ngân",
+      end_place: "nguyễn thị minh khai",
+      time: "1 tiếng 15p",
+    },
+    {
+      id: "L002",
+      name: "02",
+      start_place: "Tân Sơn Nhất",
+      end_place: "Bến Thành",
+      time: "45 phút",
+    },
+    {
+      id: "L003",
+      name: "03",
+      start_place: "Bến Thành",
+      end_place: "Chợ Lớn",
+      time: "1 tiếng",
+    },
+    {
+      id: "L004",
+      name: "04",
+      start_place: "Chợ Lớn",
+      end_place: "Gò Vấp",
+      time: "1 tiếng 20 phút",
+    },
+    {
+      id: "L005",
+      name: "05",
+      start_place: "Bình Thạnh",
+      end_place: "Quận 7",
+      time: "50 phút",
+    },
+  ];
+
+  const user = useSelector((state) => state.account);
+  const userId = user._id;
+  const mutation = useMutation({
+    mutationFn: (data) => {
+      return ScheduleService.getEmployeeTask(data);
+    },
+    onSuccess: (data) => {
+      console.log("Data received:", data);
+      if (Array.isArray(data)) {
+        setTaskItems(data);
+      } else {
+        console.error("Expected an array, but got:", data);
+      }
+    },
+
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+  useEffect(() => {
+    mutation.mutate(userId);
+  }, []);
+  useEffect(() => {
+    console.log("2", taskItems);
+  }, [taskItems]);
   const ITEMS_PER_PAGE = 10;
   const [searchWord, setSearchWord] = useState("");
   const [searchParams, setSearchParams] = useSearchParams();
@@ -78,34 +110,8 @@ const TaskPage = () => {
     setSearchWord(e.target.value);
     setSearchParams({ page: 1 });
   };
-
   // trả về các tuyến, giờ, bến xe mà nhân viên này làm
-  const taskItems = [
-    {
-      name: "003", // tên chuyến
-      scheduleId: "S001", // id lịch trình
-      station: "Bến Xe Miền Tây", // bến xe
-      time: "9h", // giờ bắt đầu
-      license_plate: "51A-123",
-      status: "Not yet",
-    },
-    {
-      name: "001", // tên chuyến
-      scheduleId: "S002", // id lịch trình
-      station: "Bến Xe Miền Đông", // bến xe
-      time: "10h", // giờ bắt đầu
-      license_plate: "51A-423",
-      status: "Done",
-    },
-    {
-      name: "002", // tên chuyến,
-      scheduleId: "S003", // id lịch trình
-      station: "Bến Xe Miền Tây", // bến xe
-      time: "11h", // giờ bắt đầu
-      license_plate: "51A-123",
-      status: "Waiting",
-    },
-  ];
+
   const [showCheckIn, setShowCheckIn] = useState(false);
   const [currentScheduleId, setCurrentScheduleId] = useState("");
   const handleCheckIn = (scheduleId) => {
@@ -199,7 +205,7 @@ const TaskPage = () => {
       </div>
       {/* Task */}
       <div
-        className={`rounded-lg p-8 shadow-md border bg-white border-gray-200 w-80`}>
+        className={`rounded-lg h-full overflow-y-auto p-8 shadow-md border bg-white border-gray-200 w-80`}>
         <div className='text-2xl font-semibold mb-4 text-white bg-gradient-to-r from-green-500 to-green-500 shadow-lg rounded-lg py-3 px-5 text-center'>
           Tasks
         </div>
@@ -208,12 +214,10 @@ const TaskPage = () => {
           {taskItems.map((item, index) => (
             <div
               key={index}
-              className={`p-5 rounded-lg shadow-md border ${
-                item.status === "Done" ? "bg-green-200" : "bg-white"
-              }  border-gray-200  transition`}>
+              className={`p-5 rounded-lg shadow-md border bg-white border-gray-200  transition`}>
               <div className='grid grid-cols-2 gap-y-2'>
                 <div className='text-lg font-medium text-gray-800 col-span-2'>
-                  Line: {item.name}
+                  {item.name}
                 </div>
                 <div className='text-sm text-gray-600'>
                   <span className='font-semibold text-gray-700'>Station:</span>{" "}
@@ -225,19 +229,19 @@ const TaskPage = () => {
                   </span>{" "}
                   {item.license_plate}
                 </div>
-                <div className='text-sm text-gray-600'>
+                <div className='text-sm flex flex-col text-gray-600'>
                   <span className='font-semibold text-gray-700'>
                     Time start:
-                  </span>{" "}
-                  {item.time}
+                  </span>
+                  <div>{item.time}</div>
                 </div>
                 <div
                   className={`text-sm font-semibold ${
-                    item.status === "Done"
+                    item.status === "Completed"
                       ? "text-green-500"
-                      : item.status === "Waiting"
+                      : item.status === "Not started yet"
                       ? "text-yellow-500"
-                      : "text-red-500"
+                      : "text-blue-500"
                   }`}>
                   <span className='text-gray-700'>Status:</span>{" "}
                   {item.status || "Unknown"}
@@ -249,11 +253,15 @@ const TaskPage = () => {
                     handleCheckIn(item.scheduleId);
                   }}
                   className={`px-4 py-2 rounded-md ${
-                    item.status === "Done"
-                      ? "bg-green-500 pointer-events-none"
-                      : "bg-yellow-400"
-                  } text-white font-medium hover:bg-yellow-300 transition`}>
-                  {item.status === "Done" ? <>Checked In</> : <>Check In</>}
+                    item.ticket3 || item.ticket7
+                      ? "bg-slate-400 pointer-events-none text-slate-800"
+                      : "bg-green-500 text-white "
+                  }  font-medium hover:bg-yellow-300 transition`}>
+                  {item.ticket3 || item.ticket7 ? (
+                    <>Checked In</>
+                  ) : (
+                    <>Check In</>
+                  )}
                 </button>
               </div>
             </div>
@@ -272,6 +280,7 @@ const TaskPage = () => {
               childCloseFormRequest={() => {
                 setShowCheckIn(false);
               }}
+              role='busboy'
               scheduleId={currentScheduleId}
             />
           </div>
